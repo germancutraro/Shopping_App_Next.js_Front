534d0da47008e18ed543bb979cfc979b6a1e1db4
German Cutraro <germancutraro@hotmail.com>
2020-03-29 19:14:57 -0300

    edit / delete options

--- components/Item/Item.jsx
+++ components/Item/Item.jsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
+
 const Item = ({ title, price, image }) => {
   return (
     <div>
       {image && <img src={image} alt='' />}
       <h3>{title}</h3>
       <span>${price}</span>
+
+      <Link href=''>
+        <a>Edit</a>
+      </Link>
+      <a href='#'>Delete</a>
     </div>
   );
 };
